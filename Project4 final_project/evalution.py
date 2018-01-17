714fb0e059b27110b753c25621049d5a41aa3927
Yu-Hsin, Chen <yaya56790@gmail.com>
2018-01-18 03:34:08 +0800

    Add a evalution method
    RSMLE method:
    Root Mean Squared Logarithmic Error.

--- Project4 final_project/evalution.py
+++ Project4 final_project/evalution.py
@@ -1,4 +1,5 @@
 from function_def import *
+import math
 import pandas as pd
 
 def readDf( fileName ):
@@ -6,23 +7,30 @@ def readDf( fileName ):
 		df = pd.read_csv( fileName , delimiter = ',')
 		return df
 
-
 if __name__ == "__main__" :
 	if len(sys.argv)!=2:
 		print("Useage error: [evalutaion output]")
 		sys.exit()
-
 
 	# timer start
 	tStart = time.time()
 
 	df = readDf( sys.argv[1] )
 	errorSum = 0.0
-	for i in range(len(df)):
-		error = abs( float(df['trip_duration'][i]) - float(df['guesstime'][i]) )
+	RSMLE = 0.0
+	n = len(df)
+	for i in range(n):
+		actual = float(df['trip_duration'][i])
+		predict = float(df['guesstime'][i])
+		error = abs( actual - predict )
+		RSMLE_each = (math.log(predict+1) - math.log(actual+1))**2
 		errorSum += error
+		RSMLE += RSMLE_each
 
+	RSMLE = sqrt(RSMLE/n)
 	# timer end
 	tEnd = time.time()
-	print("Average time error: " + str(errorSum/len(df)) )
+	print("Average time error: " + str(errorSum/n) )
+	print("RSMLE: " + str(RSMLE) )
+
 	print("Execution time: ", tEnd - tStart)
